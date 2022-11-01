d210b151775c387c12f7c60ded0fb5a3886082d5
CS Lab Account <cslab@olin310-16.mathcs.carleton.edu>
2022-10-31 23:06:22 -0500

    initial grid setup

--- World.h
+++ World.h
@@ -76,4 +76,11 @@ int count;
   }
 };
 
+ int getDemeIndex(int x, int y){
+    
+    for(int i = 0;i<6;i++){
+
+    }
+ }
+
 #endif
--- web.cpp
+++ web.cpp
@@ -1,5 +1,5 @@
 
-//in web.cpp at the VERY top
+// in web.cpp at the VERY top
 #define UIT_VENDORIZE_EMP
 #define UIT_SUPPRESS_MACRO_INSEEP_WARNINGS
 #include "emp/math/Random.hpp"
@@ -12,13 +12,12 @@
 #include "emp/web/UrlParams.hpp"
 #include "ConfigPanel.h"
 
-
 MyConfigType config;
 emp::web::Document doc{"target"};
 emp::web::Document settings("settings");
 
-
-class Animator : public emp::web::Animate {
+class Animator : public emp::web::Animate
+{
     const int num_h_boxes = 50;
     const int num_w_boxes = 50;
     const double RECT_SIDE = 10;
@@ -30,81 +29,60 @@ class Animator : public emp::web::Animate {
     OrgWorld world{random};
 
 public:
+    // Constructor for the class that sets up the Gui for the animation
+    Animator()
+    {
 
-
-//Constructor for the class that sets up the Gui for the animation
-Animator() {    
-        
-        
         auto specs = emp::ArgManager::make_builtin_specs(&config);
         emp::ArgManager am(emp::web::GetUrlParams(), specs);
         am.UseCallbacks();
-        if (am.HasUnused()) std::exit(EXIT_FAILURE);
+        if (am.HasUnused())
+            std::exit(EXIT_FAILURE);
         emp::prefab::ConfigPanel config_panel(config);
         config_panel.SetRange("Population", "1", "25");
         settings << config_panel;
         doc << canvas;
         doc << GetToggleButton("Toggle");
         doc << GetStepButton("Step");
-        doc << "\n\n\n" << "The starting organisms and their off springs are trying to solve 5 tasks defined as some Mathematical function. As they keep solving the tasks successfully, they are able to gain points which subsequently determines whether they are able to reprdocue or not. They then trasnfer their genome to their offspring which creates a favorable condition for evolution. The change in color observed as they keep reproducing represents the five different tasks.";
         random.ResetSeed(config.SEED());
         world.SetPopStruct_Grid(num_w_boxes, num_h_boxes);
-        for(int i = 0;i<config.Population();i++){
-         Organism* new_org1 = new Organism(&world, 1);
-         world.Inject(*new_org1);
+        for (int i = 0; i < config.Population(); i++)
+        {
+            Organism *new_org1 = new Organism(&world, 1);
+            world.Inject(*new_org1);
         }
 
         world.Resize(num_w_boxes, num_h_boxes);
+    }
 
-
-        
-}
-
-
-void DoFrame() override {
+    void DoFrame() override
+    {
         canvas.Clear();
         world.Update();
         int org_num = 0;
-        for (int x = 0; x < num_w_boxes; x++){
-            for (int y = 0; y < num_h_boxes; y++) {
-                if (world.IsOccupied(org_num)) {
-                    Organism organism = world.GetOrg(org_num); 
-                    for(int i = 0;i<1;i++){                         //Change the real time color of the cells depending on the task solved
-                        if(world.GetTask(i)=="abs"){
-                        canvas.Rect(x * RECT_SIDE, y * RECT_SIDE, RECT_SIDE, RECT_SIDE, "black", "black");
-                        }
-                        if(world.GetTask(i)=="trig"){
-                        canvas.Rect(x * RECT_SIDE, y * RECT_SIDE, RECT_SIDE, RECT_SIDE, "blue", "blue");
-                        }
-                        if(world.GetTask(i)=="log"){
-                        canvas.Rect(x * RECT_SIDE, y * RECT_SIDE, RECT_SIDE, RECT_SIDE, "green", "green");
-                        }
-                        if(world.GetTask(i)=="floor"){
-                        canvas.Rect(x * RECT_SIDE, y * RECT_SIDE, RECT_SIDE, RECT_SIDE, "grey", "grey");
-                        }
-                        if(world.GetTask(i)=="ceil"){
-                        canvas.Rect(x * RECT_SIDE, y * RECT_SIDE, RECT_SIDE, RECT_SIDE, "red", "red");
-                        }
-                    }
-
+        for (int x = 0; x < num_w_boxes; x++)
+        {
+            for (int y = 0; y < num_h_boxes; y++)
+            {
+                if (world.IsOccupied(org_num))
+                {
+                    std::cout << "occupied"<<std::endl;
+                    Organism organism = world.GetOrg(org_num);
+                    canvas.Rect(x * RECT_SIDE, y * RECT_SIDE, RECT_SIDE, RECT_SIDE, "black", "black");
+                    
                 }
-                else {
-                    canvas.Rect(x * RECT_SIDE, y * RECT_SIDE, RECT_SIDE, RECT_SIDE, "yellow", "yellow");
+                else{
+                    canvas.Rect(x * RECT_SIDE, y * RECT_SIDE, RECT_SIDE, RECT_SIDE, "white", "black");
                 }
                 org_num++;
             }
         }
     }
-    
 };
 
 Animator animator;
 
-int main() {
+int main()
+{
     animator.Step();
 }
-
-
-
-
-
