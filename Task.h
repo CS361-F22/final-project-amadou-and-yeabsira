decc42b03b32621f20c21d8cccecdcfafa8050b8
CS Lab Account <cslab@olin310-16.mathcs.carleton.edu>
2022-10-31 22:49:32 -0500

    starter code

---
+++ Task.h
@@ -0,0 +1,132 @@
+
+#include <cmath>
+#include <string>
+
+class Task {
+public:
+  virtual double CheckOutput(float output, float inputs[4]) = 0;
+  virtual std::string name() = 0;
+};
+
+
+class Task_1 : public Task {
+
+   public:
+   //return a double if the organism manages to fidn the trig function of the input numbers
+     double CheckOutput(float output, float inputs[4]) override {
+         for (int i = 0; i < 4; i++) {
+          float cube = cos(inputs[i]) / inputs[i];
+          if (fabs(output - cube) < 0.001) {
+              std::cout << "Cubbed!" << std::endl;
+              return 0.1;
+          }
+        }
+         return 0;
+     }
+     public: 
+     //returns the name of the task solved
+     std::string name() override{
+        std::string n = "trig";
+        return n;
+     }
+
+};
+
+
+class Task_2 : public Task {
+
+   public:
+   //return a double if the organism manages to find the log base 10 of the input numbers
+     double CheckOutput(float output, float inputs[4]) override {
+         for (int i = 0; i < 4; i++) {
+          float log = log10(inputs[i]);
+          if (fabs(output - log) < 0.001) {
+              std::cout << "Logarithm!"  << std::endl;
+              return 0.1;
+          }
+        }
+         return 0;
+     }
+    public: 
+    //returns the name of the task solved
+     std::string name()  override {
+        std::string n = "log";
+        return n;
+     }
+
+};
+
+
+
+class Task_3 : public Task {
+
+   public:
+   //return a double if the organism manages to find the absolute value of the input numbers
+     double CheckOutput(float output, float inputs[4]) override {
+         for (int i = 0; i < 4; i++) {
+          float magnitude = fabs(inputs[i])*5;
+
+          //Since these are floats, allowing for some floating point error
+          if (fabs(output - magnitude) < 0.001) {
+              std::cout << "Aboslute Value!" << std::endl;
+              return 0.1;
+          }
+        }
+         return 0;
+     }
+     public: 
+     //returns the name of the task solved
+      std::string name()  override{
+        std::string n = "abs";
+        return n;
+      }
+
+};
+
+
+class Task_4 : public Task {
+
+   public:
+   //return a double if the organism manages to find the floor of of the input numbers multiplied by the square
+     double CheckOutput(float output, float inputs[4]) override {
+         for (int i = 0; i < 4; i++) {
+          float floor_i = floor(inputs[i])*(inputs[i])*(inputs[i]);
+          if (fabs(output - floor_i) < 0.001) {
+              std::cout << "Floor Function!" << std::endl;
+              return 0.1;
+          }
+        }
+         return 0;
+     }
+
+    public: 
+    //returns the name of the task solved
+     std::string name()  override{
+        std::string n = "floor";
+        return n;
+     }
+
+};
+
+class Task_5 : public Task {
+
+   public:
+   //return a double if the organism manages to find the ceiling of the input numbers
+     double CheckOutput(float output, float inputs[4]) override {
+         for (int i = 0; i < 4; i++) {
+          float ceil_i = ceilf(inputs[i]);
+          if (fabs(output - ceil_i) < 0.001) {
+              std::cout << "Ceil Function!" << std::endl;
+              return 0.1;
+          }
+        }
+         return 0;
+     }
+   public: 
+   //returns the name of the task solved
+     std::string name()  override{
+        std::string n = "ceil";
+        return n;
+     }
+
+};
